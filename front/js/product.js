9cd79e39683093fb89f78577872d26a2c52aec51
Jysai <jysai59@gmail.com>
2021-10-07 20:13:20 +0200

    diminution de nombres de lignes avec le template literals dans le fichier product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,7 @@
 const itemImg = document.querySelector(".item__img");
-const description = document.getElementById("description");
-const price = document.getElementById("price");
 const title = document.getElementById("title");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
 const option = document.getElementById("colors");
 
 let urlParams = new URLSearchParams(location.search);
@@ -10,28 +10,34 @@ let id = urlParams.get("id");
 fetch("http://localhost:3000/api/products/" + id)
   .then((response) => response.json())
   .then((data) => {
-    let imgSofa = document.createElement("img");
-    let titleSofa = document.createElement("h1");
-    let priceSofa = document.createElement("span");
-    let descriptionSofa = document.createElement("p");
+    itemImg.innerHTML = `<img src="${data.imageUrl}">`;
+    title.textContent = `${data.name}`;
+    price.textContent = `${data.price}`;
+    description.textContent = `${data.description}`;
+    option.innerHTML = data.colors.map(c => `<option>${c}</option>`);
 
-    imgSofa.src = data.imageUrl;
-    titleSofa.innerText = data.name;
-    priceSofa.innerText = data.price;
-    descriptionSofa.innerText = data.description;
+    // let imgSofa = document.createElement("img");
+    // let titleSofa = document.createElement("h1");
+    // let priceSofa = document.createElement("span");
+    // let descriptionSofa = document.createElement("p");
 
-    itemImg.appendChild(imgSofa);
-    title.appendChild(titleSofa);
-    price.appendChild(priceSofa);
-    description.appendChild(descriptionSofa);
+    // imgSofa.src = data.imageUrl;
+    // titleSofa.innerText = data.name;
+    // priceSofa.innerText = data.price;
+    // descriptionSofa.innerText = data.description;
 
-    function choiceColorOption(color) {
-      let optionSofa = document.createElement("option");
-      optionSofa.innerText = color;
-      option.appendChild(optionSofa);
-    }
+    // itemImg.appendChild(imgSofa);
+    // title.appendChild(titleSofa);
+    // price.appendChild(priceSofa);
+    // description.appendChild(descriptionSofa);
 
-    for (i = 0; i < data.colors.length; i++) {
-      choiceColorOption(data.colors[i]);
-    }
+    // function addColorOption(color) {
+    //   let optionProduct = document.createElement("option");
+    //   optionProduct.innerText = color;
+    //   option.appendChild(optionProduct);
+    // }
+
+    // for (i = 0; i < data.colors.length; i++) {
+    //   addColorOption(data.colors[i]);
+    // }
   });
--- front/js/script.js
+++ front/js/script.js
@@ -4,11 +4,13 @@ fetch("http://localhost:3000/api/products")
   .then((response) => response.json())
   .then((data) => {
     for (i = 0; i < data.length; i++) {
+
       let article = document.createElement("article");
       let nameSofa = document.createElement("h3");
       let descriptionSofa = document.createElement("p");
       let imgSofa = document.createElement("img");
       let urlSofa = document.createElement("a");
+
 
       urlSofa.href = "product.html?id=" + data[i]._id;
       imgSofa.src = data[i].imageUrl;
@@ -24,20 +26,3 @@ fetch("http://localhost:3000/api/products")
       article.appendChild(descriptionSofa);
     }
   });
-
-// const xhr = new XMLHttpRequest();
-
-// xhr.open("GET", "http://localhost:3000/api/products");
-
-// (xhr.responseType = "json"),
-//   (xhr.onload = function () {
-//     for (i = 0; i < xhr.response.length; i++) {
-//       let newSofa = document.createElement("p");
-
-//       newSofa.innerText = xhr.response[i].name;
-
-//       nameSofa.appendChild(newSofa);
-//     }
-//   });
-
-// xhr.send();
