687aca4ad4c6a58286015efeaa147e2d1c7dac85
Jysai <jysai59@gmail.com>
2021-10-11 00:11:21 +0200

    ajout de la boucle for et condition pour afficher les produits dans la page panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,43 +1,82 @@
-const itemImg = document.querySelector(".item__img");
-const title = document.getElementById("title");
-const price = document.getElementById("price");
-const description = document.getElementById("description");
-const option = document.getElementById("colors");
+const imageSofa = document.querySelector(".item__img");
+const titleSofa = document.getElementById("title");
+const priceSofa = document.getElementById("price");
+const descriptionSofa = document.getElementById("description");
+const colorSofa = document.getElementById("colors");
+const quantitySofa = document.getElementById("quantity");
+const btnAddBasket = document.getElementById("addToCart");
+
+const basket = JSON.parse(localStorage.getItem("Sofas")) || [];
 
 let urlParams = new URLSearchParams(location.search);
-let id = urlParams.get("id");
+let idSofa = urlParams.get("id");
 
-fetch("http://localhost:3000/api/products/" + id)
+fetch("http://localhost:3000/api/products/" + idSofa)
   .then((response) => response.json())
   .then((data) => {
-    itemImg.innerHTML = `<img src="${data.imageUrl}">`;
-    title.textContent = `${data.name}`;
-    price.textContent = `${data.price}`;
-    description.textContent = `${data.description}`;
-    option.innerHTML = data.colors.map(c => `<option>${c}</option>`);
+    imageSofa.innerHTML = `<img src="${data.imageUrl}">`;
+    titleSofa.textContent = `${data.name}`;
+    priceSofa.textContent = `${data.price}`;
+    descriptionSofa.textContent = `${data.description}`;
+    colorSofa.innerHTML = data.colors.map((a) => `<option>${a}</option>`);
 
-    // let imgSofa = document.createElement("img");
-    // let titleSofa = document.createElement("h1");
-    // let priceSofa = document.createElement("span");
-    // let descriptionSofa = document.createElement("p");
+    btnAddBasket.addEventListener("click", (e) => {
+      
+      class sofa {
+        constructor(id, color, quantity, name, image, price) {
+          this.idSofa = id;
+          this.colorSofa = color;
+          this.quantitySofa = +quantity;
+          this.titleSofa = name;
+          this.imageSofa = image;
+          this.priceSofa = price;
+        }
+      }
 
-    // imgSofa.src = data.imageUrl;
-    // titleSofa.innerText = data.name;
-    // priceSofa.innerText = data.price;
-    // descriptionSofa.innerText = data.description;
+      let newProduct = new sofa(idSofa, colorSofa.value, quantitySofa.value, titleSofa.textContent, imageSofa.innerHTML, priceSofa.textContent);
+      
+      let alreadyPresent = false;
+      let modificationProduct;
+      for (product of basket) {
+        switch (product.colorSofa) {
+          case newProduct.colorSofa:
+            alreadyPresent = true;
+            modificationProduct = basket.indexOf(product);
+        }
+      }
 
-    // itemImg.appendChild(imgSofa);
-    // title.appendChild(titleSofa);
-    // price.appendChild(priceSofa);
-    // description.appendChild(descriptionSofa);
-
-    // function addColorOption(color) {
-    //   let optionProduct = document.createElement("option");
-    //   optionProduct.innerText = color;
-    //   option.appendChild(optionProduct);
-    // }
-
-    // for (i = 0; i < data.colors.length; i++) {
-    //   addColorOption(data.colors[i]);
-    // }
+      if (alreadyPresent) {
+        basket[modificationProduct].quantitySofa =
+          basket[modificationProduct].quantitySofa + newProduct.quantitySofa;
+        localStorage.setItem("Sofas", JSON.stringify(basket));
+      } else {
+        basket.push(newProduct);
+        localStorage.setItem("Sofas", JSON.stringify(basket));
+      }
+    });
   });
+
+// let imgSofa = document.createElement("img");
+// let titleSofaSofa = document.createElement("h1");
+// let priceSofaSofa = document.createElement("span");
+// let descriptionSofaSofa = document.createElement("p");
+
+// imgSofa.src = data.imageSofaUrl;
+// titleSofaSofa.innerText = data.name;
+// priceSofaSofa.innerText = data.priceSofa;
+// descriptionSofaSofa.innerText = data.descriptionSofa;
+
+// itemImg.appendChild(imgSofa);
+// titleSofa.appendChild(titleSofaSofa);
+// priceSofa.appendChild(priceSofaSofa);
+// descriptionSofa.appendChild(descriptionSofaSofa);
+
+// function addColorOption(color) {
+//   let optionProduct = document.createElement("option");
+//   optionProduct.innerText = color;
+//   option.appendChild(optionProduct);
+// }
+
+// for (i = 0; i < data.colors.length; i++) {
+//   addColorOption(data.colors[i]);
+// }
--- front/js/script.js
+++ front/js/script.js
@@ -4,13 +4,13 @@ fetch("http://localhost:3000/api/products")
   .then((response) => response.json())
   .then((data) => {
     for (i = 0; i < data.length; i++) {
+      console.log(data);
 
       let article = document.createElement("article");
       let nameSofa = document.createElement("h3");
       let descriptionSofa = document.createElement("p");
       let imgSofa = document.createElement("img");
       let urlSofa = document.createElement("a");
-
 
       urlSofa.href = "product.html?id=" + data[i]._id;
       imgSofa.src = data[i].imageUrl;
