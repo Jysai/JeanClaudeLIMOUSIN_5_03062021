239cae6b9feecd42478833dbe32156a0bd30157d
Jysai <jysai59@gmail.com>
2021-10-22 00:02:43 +0200

    ajout du total du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,7 @@
-const cartItems = document.getElementById("cart__items"); 
+const cartItems = document.getElementById("cart__items");
 let basket = JSON.parse(localStorage.getItem("Sofas")); //localStorage
-
+const totalQuantity = document.getElementById("totalQuantity");
+const totalPrice = document.getElementById("totalPrice");
 
 // Fonction pour changer la quantité d'un produit dans le panier
 function onChangeClick(id, color) {
@@ -20,15 +21,8 @@ function onChangeClick(id, color) {
   window.location.reload();
 }
 
-
 // Fonction pour supprimer un produit dans le panier
-function onDeleteClick(id, color) {
-  const article = document.querySelector(
-    `article[data-color="${color}"][data-id="${id}"]`
-  );
-  const deleteItem = article.querySelector("p.deleteItem");
-  console.log(deleteItem);
-
+function onClickDelete(id, color) {
   for (const product of basket) {
     if (product.color == color && product.id == id) {
       modificationProduct = basket.indexOf(product);
@@ -42,28 +36,8 @@ function onDeleteClick(id, color) {
   window.location.reload();
 }
 
-function totalPrice() {
-  //creation d'un tableau qui contiendra chaque prix du panier
-  let prixPanier = [];
-  //On boucle sur tout les produit trouver dans le panier
-  for (produit of userPanier) {
-    //On transforme les string prix en number
-    let prixTotal = parseInt(produit.prix) * parseInt(produit.quantite);
-    prixPanier.push(prixTotal);
-  }
-  //On utilise la méthode reduce pour additioner tout nos prix du panier
-  const calculTotal = (acc, curr) => acc + curr;
-  document.getElementById("totalPrice").textContent =
-    prixPanier.reduce(calculTotal);
-}
-
-
-
-
-
-
 // Boucle qui permet de récuperer les informations dans le localstorage puis affiche un récapitulatif dans la page Panier
-if (basket === null || basket === []) {
+if (basket === null) {
   cartItems.innerHTML = `
     <article class="cart__item">    
                 <h1>Votre panier est vide</h1>
@@ -74,28 +48,63 @@ if (basket === null || basket === []) {
     fetch("http://localhost:3000/api/products/" + basket[i].id)
       .then((response) => response.json())
       .then((data) => {
-        const productPriceAndQuantity = data.price * basket[i].quantity
-        cartItems.innerHTML += `<article class="cart__item" data-id=${basket[i].id} data-color=${basket[i].color}>
+        cartItems.innerHTML += `<article class="cart__item" data-id=${
+          basket[i].id
+        } data-color=${basket[i].color} data-price=${
+          data.price * basket[i].quantity
+        } data-quantity=${basket[i].quantity}>
           <div class="cart__item__img">
           <img src=${data.imageUrl}>
           </div>
           <div class="cart__item__content">
             <div class="cart__item__content__titlePrice">
               <h2>${data.name}</h2>
-              <p>${productPriceAndQuantity}€</p>
+              <p >${data.price * basket[i].quantity}€</p>
             </div>
             <div class="cart__item__content__settings">
               <p>Couleur : ${basket[i].color}</p>
               <div class="cart__item__content__settings__quantity">
                 <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${basket[i].quantity} onchange=onChangeClick('${basket[i].id}','${basket[i].color}')>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${
+                  basket[i].quantity
+                } onchange=onChangeClick('${basket[i].id}','${
+          basket[i].color
+        }')>
               </div>
               <div class="cart__item__content__settings__delete">
-                <p class="deleteItem" onclick=onDeleteClick('${basket[i].id}','${basket[i].color}')>Supprimer</p>
+                <p class="deleteItem" onclick=onClickDelete('${
+                  basket[i].id
+                }','${basket[i].color}')>Supprimer</p>
               </div>
             </div>
           </div>
           </article>`;
       });
   }
+}
+
+// Permet d'additioner la quantité total des produits dans le panier sous forme d'un tableau avec la méthode reduce()
+let arrayQ = [];
+for (const product of basket) {
+  let itemQuant = product.quantity;
+  arrayQ.push(itemQuant);
+}
+totalQuantity.textContent = arrayQ.reduce(
+  (accumulator, curr) => accumulator + curr
+);
+
+// Le total du panier
+const arrayP = [];
+for (let i = 0; i < basket.length; i++) {
+  fetch("http://localhost:3000/api/products/" + basket[i].id)
+    .then((response) => response.json())
+    .then((data) => {
+      const totalPriceBasket = basket.reduce(() => {
+        return data.price * basket[i].quantity;
+      }, 0);
+      arrayP.push(totalPriceBasket);
+      totalPrice.textContent = arrayP.reduce(
+        (accumulator, curr) => accumulator + curr
+      );
+    });
 }
